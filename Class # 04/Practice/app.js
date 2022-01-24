145d2175f9dd55053d1166bba33cfa650cf8c434
UmerSethi <m.umersethi91@gmail.com>
2022-01-24 23:44:02 +0500

    Practice

---
+++ Class # 04/Practice/app.js
@@ -0,0 +1,136 @@
+                //   CHAPTER # 21
+
+
+            // ===========================
+
+                  //  CHANGING CASE
+
+            // ===========================
+
+// var City = prompt("Enter Your City Name : ")
+// var arr = ["Karachi" , "Isalamabad" , "Peshawar"]
+
+// for (var i = 0 ; i < City.length ; i++){
+//     if(arr[i] === City){
+//         alert("City Has Found")
+//     }else{
+//         alert("City is Not Found !")
+    // }
+// }
+
+                        //   OR  
+
+
+// var City = prompt("Enter Your City Name : ")
+
+// City = City.toLowerCase()                                        //toLowerCase()
+
+// var arr = ["karachi" , "isalamabad" , "peshawar"]
+
+// for (var i = 0 ; i < City.length ; i++){
+//     if(arr[i] === City){
+//         alert("City Has Found")
+//     }else{
+//         alert("City is Not Found !")
+//     }
+// }
+
+
+
+
+
+// var City = prompt("Enter Your City Name : ")
+
+// City = City.toUpperCase()                                        //toUpperCase()
+
+// var arr = ["KARACHI" , "ISLAMABAD" , "PESHAWAR"]
+
+// for (var i = 0 ; i < City.length ; i++){
+//     if(arr[i] === City){
+//         alert("City Has Found")
+//     }else{
+//         alert("City is Not Found !")
+//     }
+// }
+
+
+
+
+
+
+// =====================================================================================
+
+
+
+
+            
+    // ==================================================== ==========================
+  //             CHAPTER # 22 :- STRINGS MEASURING LENGTH & EXTRACTING PARTS
+  //    ===============================================================================
+
+//            WHAT IF USER TYPE UPPER & LOWER CASE BOTH IN ONE WORD?  
+  
+
+
+// var City = prompt("Enter Your City Name : ")
+// var firstChar = City.slice(0,1);                          
+//  firstChar = firstChar.toUpperCase()
+// var otherChar = City.slice(1);
+// otherChar = otherChar.toLowerCase()
+
+// var City = firstChar + otherChar ;
+// var found = "false"
+
+// var arr = ["Karachi" , "Islamabad" , "Peshawar"]
+
+// for (var i = 0 ; i < City.length ; i++){
+//     if(arr[i] === City){
+   
+//         alert("City Has Found")
+//         found = "true"
+               
+//     }
+    
+// }
+
+// if(found==="false"){
+//     alert("not found")
+// }
+
+
+
+// ---------------------------------------------------------------------------------------------
+
+
+// QUESTION :-
+
+//  WRITE A CODE TO DETECT DOUBLE SPACE IN A SENTENCE ?    // ALERT IF USER USE DOUBLE SPACE IN A SENTENCE
+
+
+
+// var str = prompt("Enter some text ....")
+// var numChars = str.length;
+
+// for( var i = 0 ; i < numChars ; i++){
+//     if (str.slice(i , i+2 )==="  "){
+//         alert("Double space are Found !")
+//     }
+// }
+
+
+
+
+
+
+// =====================================================================================
+
+
+
+
+
+
+
+//     ===============================================================================
+//             CHAPTER # 23 :- STRINGS FINDING SEGMENTS
+//     ===============================================================================
+
