439c8a479af0ac85b1f5470a178c0ad096ae48ca
UmerSethi <m.umersethi91@gmail.com>
2022-01-25 00:33:37 +0500

    Practice

--- Class # 04/Practice/app.js
+++ Class # 04/Practice/app.js
@@ -135,3 +135,9 @@
 //     ===============================================================================
 
 // CONTINUE.......
+
+for (var i = 0; i < text.length; i++) {
+    if (text.slice(i, i + 12) === "World War II") {
+     text = text.slice(0, i) + "the Second World War" + text.slice(i + 12);
+    }
+     }
