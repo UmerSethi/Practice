fbcdeb7e3dac057256d7558b4d00c6b99da71c19
UmerSethi <m.umersethi91@gmail.com>
2022-01-31 15:50:27 +0500

    Practice

---
+++ Class # 07/Practice/app.js
@@ -0,0 +1,118 @@
+//           ==============================================================
+//                  CHAPTER # 51 :- Reading and setting paragraph text
+//           ==============================================================
+
+
+
+// <!-- ====> FIRST METHOD :- -->
+
+// function readMore(){
+
+// var text = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus ea cumque soluta. Aliquid, natus nostrum laborum similique quam eum quisquam fugiat quos , aperiam reiciendis cumque deleniti esse, sapiente repellendus  eius ? "
+
+// var paragraph = document.getElementById('para')
+
+// paragraph.innerHTML = text
+
+// }
+
+
+
+// <!-- ----------------------------------------------------------------------------------------------- -->
+
+
+
+// //  <!-- ====> SECOND METHOD :- -->
+
+// function readMore(){
+//     var FIRST = document.getElementById('text').innerHTML
+
+//     var SECOND = document.getElementById('para')
+
+//     SECOND.innerHTML = FIRST
+// }
+
+
+
+// <!-- ----------------------------------------------------------------------------------------------- -->
+
+
+// <!-- ====> PRACTICE :- -->
+
+// function readMore() {
+//     var text =" Muhammad Umer Saleem "
+
+//     var completeName = document.getElementById('name')
+
+//     completeName.innerHTML = text
+
+
+// }
+
+
+// <!-- ----------------------------------------------------------------------------------------------- -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// _EXAMPLE #01  :-
+
+// function readMore() {
+    
+//     var text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem reiciendis nemo atque veritatis architecto vel deserunt harum quo! Alias odio nisi tenetur aliquid nesciunt iste aperiam eligendi asperiores dignissimos itaque?"
+
+//     var paragraph = document.getElementById('para')
+
+//     paragraph.innerHTML = text
+// }
+
+
+
+
+// <!-- ----------------------------------------------------------------------------------------------- -->
+
