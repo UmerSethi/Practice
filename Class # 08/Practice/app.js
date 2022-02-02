f3e5085313bbe43085f65b47d1f7efb891037b2a
UmerSethi <m.umersethi91@gmail.com>
2022-02-02 21:06:24 +0500

    Practice

---
+++ Class # 08/Practice/app.js
@@ -0,0 +1,108 @@
+
+
+
+// (1) => .innerHTML     //Only for <p></p>,<span></span>,<inner>,<inner>,input (acha input jo h ".innerText")
+
+// (2) => .innerText      //ONLY for Text
+
+// (3) => .value         //for input (.innerHTML      also)
+
+
+
+
+// -------------------------------------------------------------------------------------------
+
+
+// function getValue(){
+// alert('test')                                         
+
+// }
+
+// function setValue(){
+// var name = document.getElementById('name')
+
+// name.value = 'Muhammad Umer Saleem'
+
+// }
+
+
+// ===========================================================================================
+
+
+
+
+
+// function setValue() {
+//     var para = document.getElementById('para') ;
+//     para.innerHTML = "This Paragraph is written by me ..." ;
+// }
+
+
+
+
+
+// ===========================================================================================
+
+
+
+// function bigImage(){
+// var image = document.getElementById('car-image')
+// image.className = 'big'                                        // how to give class 
+
+// }
+
+
+// function smallImage(){
+// var image =document.getElementById('car-image')
+// image.className = 'small'
+
+// }
+
+
+
+
+
+
+// ===========================================================================================
+
+
+// function onBulb(){
+//  var bulbOn = document.getElementById('Bulb-ON')
+
+//  bulbOn.src= "./image/Bulb-ON.jpeg"
+
+// }
+
+// function offBulb(){
+//     var bulbOFF = document.getElementById('Bulb-ON')
+   
+//     bulbOFF.src= "./image/Bulb-OFF.jpeg"
+   
+//    }
+   
+
+
+
+// ===========================================================================================
+
+
+
+function addClass(){
+
+    var para = document.getElementById('para')
+    para.className = "bold"
+
+}
+
+
+// ===========================================================================================
+
+
+//  QUESTION :- HOW TO GIVE MULTIPLE CLASSES ? 
+
+function addClass(){
+
+    var para = document.getElementById('para')
+    para.className += " bold blue"                        //Assigining operator sy phly "+=" lagy ga or phir " k baad space simple
+
+}
