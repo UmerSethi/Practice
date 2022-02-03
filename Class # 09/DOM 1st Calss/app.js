d6c6a33f4b6d028fb87f8b29a652d0e49e17514c
UmerSethi <m.umersethi91@gmail.com>
2022-02-04 00:28:32 +0500

    Practice

--- Class # 09/DOM 1st Calss/app.js
+++ Class # 09/DOM 1st Calss/app.js
@@ -44,14 +44,71 @@
 // ==================================================================================
 
 
-var a = document.getElementById('pappa')
-console.log(a.childNodes[1])
+// var a = document.getElementById('pappa')
+// console.log(a.childNodes[1])
 
 // OR
 
-var a = document.getElementById('pappa')
-console.log(a.firstChild)
+// var a = document.getElementById('pappa')
+// console.log(a.firstChild)
+
+ // OR
+
+
+// var a = document.getElementById('pappa')
+// console.log(a.childNodes)                   //jitny bhi child h ...en childes ko get krny k liye 
+
+
+// -------------------------------------------------------------------------------------
+
+
+// var a = document.getElementById('firstSon')
+// console.log(a.nextSibling)
+
+
+// -------------------------------------------------------------------------------------
+
+
+
+// var a = document.getElementById('pappa')
+// console.log(a.lastChild)
 
 
 
 // -------------------------------------------------------------------------------------
+
+// var a = document.getElementById('secondSon')
+// console.log(a.parentNode)
+
+
+// ==================================================================================
+
+
+// FOR NODETYPE :-
+
+//  var a = document.getElementById('firstSon')
+//  console.log(a.nodeType)
+
+//  RESULT :- 1
+
+
+// -------------------------------------------------------------------------------------
+
+
+// var a = document.getElementById('firstSon')
+// console.log(a.childNodes[0].nodeType)
+
+// -------------------------------------------------------------------------------------
+
+// var a = document.getElementById('firstSon')
+// console.log(a.nodeName)
+
+
+
+// ==================================================================================
+
+
+// FOR NODEVALUE :-
+
+// var a = document.getElementById('pappa')
+// console.log(a.childNodes[0].nodeValue)
