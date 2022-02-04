7272d538bc1c3edc790f4458ab484894cfe9baab
UmerSethi <m.umersethi91@gmail.com>
2022-02-05 01:42:32 +0500

    Practice

--- Class # 11/Todo App/app.js
+++ Class # 11/Todo App/app.js
@@ -1,1 +1,21 @@
-{/* <h1>Todo App</h1> */}
+
+var list = document.getElementById('list')
+
+function addTodo(){
+    
+    var todo_item = document.getElementById('todo-item')
+
+    var li = document.createElement('li')
+    var liText = document.createTextNode(todo_item.value)
+
+    li.appendChild(liText)
+
+    li.appendChild(li)
+
+    console.log(li)
+  
+
+}
+
+
+
