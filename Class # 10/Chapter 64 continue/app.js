2b54595212668bcc58ecee056ead0edc4a108c43
UmerSethi <m.umersethi91@gmail.com>
2022-02-04 17:05:34 +0500

    Practice

---
+++ Class # 10/Chapter 64 continue/app.js
@@ -0,0 +1,167 @@
+
+      // ================================================================
+               //    CHAPTER # 64 The DOM  Counting elements
+      // ================================================================
+
+
+
+// var list = document.getElementById("li1")
+
+// console.log(list)
+
+
+// ------------------------------------------------------------------------
+
+
+// var list = document.getElementsByClassName("li")
+
+// console.log(list)
+
+
+// ------------------------------------------------------------------------
+
+
+// var list = document.getElementsByTagName("li")
+
+// console.log(list[2])
+
+
+// ------------------------------------------------------------------------
+
+// var list = document.getElementsByTagName("li")
+
+// console.log(list.length)
+
+
+
+
+
+
+//  =====================================================================================
+
+
+            
+         // ================================================================
+                //    CHAPTER # 65:- The DOM : Attributes
+         // ================================================================ 
+
+
+        //  var target = document.getElementById('p1')
+
+        //  if(target.hasAttribute('class')){
+        //      alert('available')
+        //  }else {
+        //      alert('not available')
+        //  }
+
+
+// ------------------------------------------------------------------------
+
+
+    // var target = document.getElementById('p1')
+
+    // console.log(target.getAttribute('class'))
+
+// ------------------------------------------------------------------------
+
+
+
+    // var target = document.getElementById('p1')
+
+    // console.log(target.setAttribute("class" , "blue"))           
+
+
+// ------------------------------------------------------------------------
+
+
+
+
+
+
+//  =====================================================================================
+
+
+          
+         // =====================================================================
+                //  CHAPTER # 66:- The DOM : Attributes names and values
+         // =====================================================================
+
+
+
+
+
+    // var target = document.getElementById('p1')
+    // console.log(target.attributes)
+
+
+
+
+// ------------------------------------------------------------------------
+
+
+
+
+// var target = document.getElementById('p1')
+
+
+// console.log(target.attributes[0])
+
+// console.log(target.attributes[1])
+
+// console.log(target.attributes[2])
+
+
+
+
+
+// ------------------------------------------------------------------------
+
+
+
+
+// var target = document.getElementById('p1')
+
+
+// console.log(target.attributes[0].nodeName)
+
+// console.log(target.attributes[1].nodeName)
+
+// console.log(target.attributes[2].nodeName)
+
+
+
+
+
+// ------------------------------------------------------------------------
+
+
+
+
+
+// var target = document.getElementById('p1')
+
+
+// console.log(target.attributes[0].nodeValue)
+
+// console.log(target.attributes[1].nodeValue)
+
+// console.log(target.attributes[2].nodeValue)
+
+
+
+
+
+
+
+//  =====================================================================================
+
+
+          
+         // =============================================================
+                //  CHAPTER # 67:- The DOM : Adding nodes                    <===       "IMPORTANT CHAPTER"
+         // =============================================================
+
+
+
+
+
