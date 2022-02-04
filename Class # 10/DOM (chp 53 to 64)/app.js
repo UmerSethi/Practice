e0618bea8b5de5f8e7d6a63c1275bbc805ab4092
UmerSethi <m.umersethi91@gmail.com>
2022-02-04 16:07:01 +0500

    Practice

---
+++ Class # 10/DOM (chp 53 to 64)/app.js
@@ -0,0 +1,80 @@
+
+// <!-- QUESTION : DISABLE CONCEPT !!! -->
+
+
+
+// function disable(){
+//     var btn = document.getElementById('example')
+//     btn.disabled = true
+
+// }
+
+
+//  ================================================================================== 
+
+
+// function get(){
+//     var p =document.getElementsByTagName('p')
+//     console.log(p)
+// }
+
+
+// ----------------------------------------------------------------------------------
+
+
+
+// function get(){
+//     var p =document.getElementsByTagName('p')
+//     console.log(p[0])
+// }
+
+
+
+// ----------------------------------------------------------------------------------
+
+
+// function get(){
+    
+//     console.log(document.childNodes)
+// }
+
+
+// ----------------------------------------------------------------------------------
+
+
+
+// function get(){
+    
+//     console.log(document.childNodes[0])
+// }
+
+// ----------------------------------------------------------------------------------
+
+
+// function get(){
+    
+//     console.log(document.childNodes[1])
+// }
+
+
+
+//  ================================================================================== 
+           
+           
+                    //  ADD TO CART FUNCTION
+
+function addtocart(){
+
+    var img = document.getElementById('product')
+    var name =document.getElementById('name')
+    var price =document.getElementById('price')
+
+    var cartimg = document.getElementById('cart-product')
+    var cartname =document.getElementById('cart-name')
+    var cartprice =document.getElementById('cart-price')
+
+    cartname.innerHTML = name.innerHTML
+    cartprice.innerHTML = price.innerHTML
+    cartimg.src = img.src
+
+}
