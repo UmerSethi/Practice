d1345e64ce64446bcec721f7e9a07ab28199f620
UmerSethi <m.umersethi91@gmail.com>
2022-03-02 14:00:24 +0500

    Practice

--- ES6_First Class(01)/Practice/app.js
+++ ES6_First Class(01)/Practice/app.js
@@ -8,3 +8,22 @@ function foo(){
 foo()
 
 console.log(name)
+
+
+
+// =====================================
+
+
+var name = "Basit"
+
+function foo(){
+   name = "Ghous"
+}
+
+foo()
+
+console.log(name)
+
+
+
+// =====================================
