bd957dabe995a6119e6a967725d1223b9e3a8179
UmerSethi <m.umersethi91@gmail.com>
2022-03-05 23:54:48 +0500

    Practice

--- ES6_First Class(01)/Practice/app.js
+++ ES6_First Class(01)/Practice/app.js
@@ -998,3 +998,201 @@ console.log("Number 1")
 console.log("Number 2")
 
 console.log("Number 3")
+// var name = "Basit"
+
+// function foo(){
+//    name = "Ghous"
+// }
+
+// foo()
+
+// console.log(name)
+
+
+
+// ==================================================================================
+
+// if(true){
+
+//    let name = "ghous"
+
+// }
+
+// console.log(name)
+
+
+// =================================================================================
+
+                // =====================================
+                    //    VARIABLES var,let,const
+                // =====================================
+
+
+
+
+// const school = "SAYLANI"
+
+// var obj = {
+//     schoolName: school
+// }
+
+// console.log(obj)
+
+
+// =======================================================================================
+
+
+
+               
+                //==================================================
+                     //          Template Literals
+                //==================================================
+
+
+
+
+// let firstName = "Ghous";
+// let lastName = "Ahmed";
+
+// console.log(firstName +" "+lastName)
+// // OR
+// console.log(`My name is ${firstName} ${lastName}`)
+
+
+
+//  =======================================================================================
+
+
+
+               
+                //==================================================
+                     //        Spread Operators
+                //==================================================
+
+// var student1 = ["ghous","basit"]
+// var student2 = ["ahmed","umair"]
+
+
+
+
+//  =======================================================================================
+
+
+
+// var student1 = ["ghous","basit"]
+
+// var student2 = ["ahmed","umair"]
+
+// var merge = student1.concat(student2)
+
+// console.log(merge)
+  
+
+// OR
+
+
+// var student1 = ["ghous","basit"]
+
+// var student2 = [...student1,"ahmed","umair"]            //   "..." triple dot sy murad pechly array m jitny bhi elements h oun sb ko get kr ly ga ...+pooray array ko get nhi kry ga sirf elements ko + merge krdy ga 
+
+// console.log(student2)
+
+// OR
+
+
+// var student1 = {name: "ghous"}
+
+// var student2 = {...student1,school:"saylani"}
+
+// console.log(student2)
+
+
+
+//  =======================================================================================
+
+
+    
+                //==================================================
+                     //   OBJECT DESTRUCTURING
+                //==================================================
+
+
+// var student = {
+//     name: "ghous",
+//     school: "saylani",
+//     roll:123
+// }
+
+// console.log(student.name)
+
+// // OR
+
+
+// var student = {
+//     name: "ghous",
+//     school: "saylani",
+//     roll:123
+// }
+
+// let {name,school,roll} = student;
+
+// console.log(name)
+// console.log(school)
+// console.log(roll)
+
+
+
+
+//  =======================================================================================
+
+
+    
+                //==================================================
+                //                 Ternary Operator
+                //==================================================
+
+
+
+// var age = 20;
+
+// if (age>30){
+//     console.log("30 bara hai")
+// }else{
+//     console.log("30 sy chota hai")
+// }
+
+
+
+// OR
+
+
+
+
+// var age = 40 ;
+
+// var check = 
+// age > 30 ?
+// "30 sy bara hai"
+// :
+// "30 sy chota hai";
+// console.log(check)
+
+
+// OR
+
+
+//  =======================================================================================
+
+
+
+        
+     //        =================================================
+     //                    Async or sync
+     //        =================================================
+
+     console.log("Number 1")
+
+     console.log("Number 2")
+     
+     console.log("Number 3")
+     
