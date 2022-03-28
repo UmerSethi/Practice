a914bf54874a86d7d9acd37a2c0ceb90a1f929d1
UmerSethi <m.umersethi91@gmail.com>
2022-03-28 13:42:50 +0500

    Practice

--- ES6_Second Class(02)/Practice/app.js
+++ ES6_Second Class(02)/Practice/app.js
@@ -60,7 +60,6 @@
 // ============================================================================================
 // ============================================================================================
 
-// ============================================================================================
 
 
 
@@ -70,15 +69,233 @@
                     //  ====================================  
 
 
-let hello = ()=>{
-    console.log("Hi i am arrow function !")
-}
+// let hello = ()=>{
+//     console.log("Hi i am arrow function !")
+// }
 
-hello()
-
-
+// hello()
 
 
+// ------------------------------------------
+
+// let hello = name=>{
+//     console.log(`hello ${name}`)
+// }
+
+// hello("Umer")
+
+// ------------------------------------------
+
+
+// let hello = (name1,name2) => {
+//     console.log(`Hello ${name1} and ${name2}`)
+// }
+
+// hello("Umer","Ali")
+
+// ------------------------------------------
+
+// let hello = () => {
+//     return "Umer"
+// }
+
+// hello()
+
+// ------------------------------------------
+
+
+// let hello = () => "Umer"
+
+// console.log(hello())
 
 
 
+// ============================================================================================
+
+
+
+
+// setInterval(()=> console.log("hello"),1000)
+
+
+
+// ============================================================================================
+
+
+
+
+
+                    //  ====================================   
+                        //      Array Functions 
+                    //  ====================================  
+
+
+
+// let arr = [{name: "ghous", age: 20}, {name: "basit", age:30}]
+
+
+// let filter = arr.filter(function(a){
+//     return true
+// })
+
+// console.log(filter)
+
+
+// ------------------------------------------------------------
+
+
+
+// let arr = [{name: "ghous", age: 20}, {name: "basit", age:30}]
+
+
+// let filter = arr.filter(function(a){
+//     return a.age === 20
+// })
+
+// console.log(filter)
+
+
+// ------------------------------------------------------------
+
+
+
+// let arr = [{name: "ghous", age: 20}, {name: "basit", age:30}]
+
+
+// let filter = arr.filter( a => false)
+
+// console.log(filter)
+
+// ------------------------------------------------------------
+
+
+
+
+// let arr = [{name: "ghous", age: 20}, {name: "basit", age:30}]
+
+
+// let filter = arr.filter(a => a => true)
+
+// console.log(filter)
+
+// ------------------------------------------------------------
+
+//  let arr = [{name: "ghous", age: 20}, {name: "basit", age:30}]
+
+
+// let filter = arr.filter(a => a.age === 20 )
+
+// console.log(filter)
+
+// ------------------------------------------------------------
+
+
+// let arr = [{name: "ghous", age: 20}, {name: "basit", age:30}]
+
+
+// let filter = arr.filter(a => a.age > 10 )
+
+// console.log(filter)
+
+
+// ------------------------------------------------------------
+
+// ============================================================================================
+
+
+
+
+
+                    //  ====================================   
+                        //      Search 
+                    //  ====================================  
+
+
+
+// let name = "ghous"
+// let search ="gh"
+
+// console.log(name.startsWith(search))
+
+
+// ------------------------------------------------------------
+
+// let name = "ghous";
+// let search = "";
+
+// if (search.startsWith(name)){
+//     console.log(name)
+// }
+
+
+// ------------------------------------------------------------
+
+
+// let name = "ghous";
+// let search = "g";
+
+// if (search.startsWith(name)){
+//     console.log(name)
+// }
+
+
+// ------------------------------------------------------------
+
+
+// let name = "ghous";
+// let search = "s";
+
+// if(name.endsWith(search)){
+//     console.log(name)
+// }
+
+
+
+
+// ============================================================================================
+
+
+
+
+
+                    //  ====================================   
+                        //       Array Map
+                    //  ====================================  
+
+
+// let arr = [2,3,4,5];
+
+// console.log(arr)
+
+// let multiply = arr.map(a => a * 2)
+
+// console.log(multiply)
+
+// ------------------------------------------------------------
+
+
+// let arr = [{name: "ghous"},{name: "basit"}];
+
+// console.log(arr)
+
+// let multiply = arr.map(a => a.name = "umair")
+
+// console.log(multiply)
+
+// ------------------------------------------------------------
+
+
+// let arr = [{name: "ghous"},{name: "basit"}];
+
+// console.log(arr)
+
+// let multiply = arr.map(a => {
+//     document.getElementById("table").innerHTML = a.name
+// })
+
+// console.log(multiply)
+
+
+
+
+// ------------------------------------------------------------
